bc6fd561fe25d09bfe925f84bf9f4b60967aeeb3
moi <jerome@moi.com>
2020-02-22 19:21:28 +1100

    renaming annotation

--- src/main/java/com/water_order/rest/water_order.java
+++ src/main/java/com/water_order/rest/water_order.java
@@ -3,7 +3,7 @@ package com.water_order.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication; 
 
-@water_order 
+@SpringBootApplication
 public class water_order {
 
     public static void main(String[] args) {
