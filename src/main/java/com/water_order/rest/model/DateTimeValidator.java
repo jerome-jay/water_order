19d25cfdf3560858fc16bde65e0705c25cea2c1d
moi <jerome@moi.com>
2020-02-25 20:20:16 +1100

    adding import

--- src/main/java/com/water_order/rest/model/DateTimeValidator.java
+++ src/main/java/com/water_order/rest/model/DateTimeValidator.java
@@ -2,7 +2,11 @@ package com.water_order.rest.model;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.time.format.DateTimeFormatter;
+//import java.time.format.DateTimeFormatter;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class DateTimeValidator implements ConstraintValidator<ValidDateTime, String> {
   private String pattern;
